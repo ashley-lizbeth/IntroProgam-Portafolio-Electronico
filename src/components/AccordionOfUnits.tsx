406187cf39d61ab16a2265442b9f70249565d5c3
Ashley <ashley.liz2605@gmail.com>
2024-11-07 19:17:10 -0600

    Created base structure

---
+++ src/components/AccordionOfUnits.tsx
@@ -0,0 +1,27 @@
+import Accordion from "react-bootstrap/Accordion";
+import units from "../units";
+
+export default function AccordionOfUnits() {
+  return (
+    <Accordion>
+      {units.map((unit, unit_index) => {
+        return (
+          <Accordion.Item eventKey={unit_index.toString()}>
+            <Accordion.Header>{unit.title}</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                {unit.activities.map((activity, activity_index) => (
+                  <li>
+                    <a href={`/${unit_index + 1}/${activity_index + 1}`}>
+                      {activity}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+        );
+      })}
+    </Accordion>
+  );
+}
---
+++ src/units.ts
@@ -0,0 +1,60 @@
+interface Unit {
+  title: string;
+  activities: string[];
+}
+
+const units: Unit[] = [
+  {
+    title: "Unidad 1. Introducción y conceptos básicos",
+    activities: [
+      "Computadora y lenguajes de una computadora",
+      "Conversiones",
+      "Algoritmo",
+      "Programación estructurada",
+      "Fases de la creación de un programa",
+    ],
+  },
+  {
+    title:
+      "Unidad 2. Fundamentos de programación: operadores, tipos de datos, variables",
+    activities: [
+      "Antecedentes y definición de lenguaje C",
+      "Tipos de datos en lenguaje C",
+      "Bibliotecas en lenguaje C (librerías)",
+    ],
+  },
+  {
+    title: "Unidad 3. Estructura de control secuenciación",
+    activities: ["Estructura de control secuenciación"],
+  },
+  {
+    title: "Unidad 4. Estructura de control selectiva: simple, doble, múltiple",
+    activities: [
+      "Estructura de control selectiva simple y doble",
+      "Estructura de Control Selectiva Múltiple",
+      "Selectiva Anidada",
+    ],
+  },
+  {
+    title:
+      "Unidad 5. Estructura de control repetición: mientras, hacer-mientras, para (desde)",
+    activities: [
+      "Estructura de control repetitiva mientras (while)",
+      "Ejercicios adicionales de la estructura mientras / while",
+      "Estructura de control repetitiva hacer-mientras / do-while",
+      "Estructura de control repetitiva desde / for",
+      "Contador, acumulador y bandera",
+      "Estructuras repetitivas anidadas",
+    ],
+  },
+  {
+    title: "Unidad 6. Manejo de funciones sin y con retorno de valor",
+    activities: [
+      "Definiciones",
+      "Funciones sin devolución de datos",
+      "Funciones con devolución de datos",
+    ],
+  },
+];
+
+export default units;
